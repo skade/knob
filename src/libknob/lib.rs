d1006a675cde266a1b0d71d998cd46b147fd0c93
Florian Gilcher <florian.gilcher@asquera.de>
2013-10-01 16:09:40 +0200

    Implement a testsuite

--- src/libknob/lib.rs
+++ src/libknob/lib.rs
@@ -8,10 +8,12 @@
 extern mod extra;
 
 use std::hashmap::*;
-use std::rt::io::net::ip::{SocketAddr, IpAddr, Ipv4Addr};
 use std::os;
-use extra::getopts::groups::*;
 
+use extra::getopts::groups::{OptGroup,getopts,usage};
+use extra::getopts::Fail_;
+
+// The settings structure we save the options and settings in.
 #[deriving(Clone)]
 pub struct Settings {
   priv store: HashMap<~str,~str>,
@@ -21,28 +23,6 @@ pub struct Settings {
 impl Settings {
   pub fn new() -> Settings {
     Settings { store: HashMap::new(), options: ~[] }
-  }
-
-  pub fn load_args(&mut self) {
-    debug!("compiling options");
-
-    let args = os::args();
-    let ref prog_name = args[0];
-
-    self.set("knob.progname", prog_name.clone());
-
-    let matches = match getopts(args.tail(), self.options) {
-      Ok(m) => { m }
-      Err(f) => { fail!(f.to_err_msg()) }
-    };
-
-    debug!(matches);
-
-    let given_options = self.options.clone();
-    for opt in given_options.iter() {
-      let opt_strings = &[opt.short_name.clone(), opt.long_name.clone()];
-      self.set_opt(opt.long_name.clone(), matches.opts_str(opt_strings))
-    };
   }
 
   pub fn opt(&mut self, opt: OptGroup) {
@@ -70,45 +50,143 @@ impl Settings {
       None => { None }
     }
   }
-}
 
-pub trait SocketSettings {
-  fn socket(&self) -> SocketAddr;
-  fn port(&self) -> u16;
-  fn ip(&self) -> IpAddr;
-}
-
-impl SocketSettings for Settings {
-  fn socket(&self) -> SocketAddr {
-    do self.fetch_with("addr") |addr| {
-      match addr {
-        Some(socket_addr) => { socket_addr },
-        None => {
-          let port: u16 = self.port();
-          let ip: IpAddr = self.ip();
-          SocketAddr { ip: ip, port: port }
-        }
-      }
-    }
+  pub fn load_os_args(&mut self) {
+    let args = os::args();
+    self.load_args(args);
   }
 
-  fn port(&self) -> u16 {
-    self.fetch("port").unwrap_or(8080)
+  pub fn load_args(&mut self, args: ~[~str]) -> Option<Fail_> {
+    let ref prog_name = args[0];
+
+    self.set("knob.progname", prog_name.clone());
+
+    let matches = match getopts(args.tail(), self.options) {
+      Ok(m) => { m }
+      Err(fail) => { return Some(fail) }
+    };
+
+    debug!(matches);
+
+    let given_options = self.options.clone();
+    for opt in given_options.iter() {
+      let opt_strings = &[opt.short_name.clone(), opt.long_name.clone()];
+      self.set_opt(opt.long_name.clone(), matches.opts_str(opt_strings))
+    };
+    None
   }
 
-  fn ip(&self) -> IpAddr {
-    self.fetch("ip").unwrap_or(Ipv4Addr(127,0,0,1))
+  pub fn usage(&self, brief: &str) -> ~str {
+    usage(brief, self.options)
   }
 }
 
 #[cfg(test)]
 mod tests {
   use super::*;
+  use std::rt::io::net::ip::{SocketAddr, IpAddr, Ipv4Addr};
+  use extra::getopts::groups::*;
+
+  pub trait SocketSettings {
+    fn socket(&self) -> SocketAddr;
+    fn port(&self) -> u16;
+    fn ip(&self) -> IpAddr;
+  }
+
+  impl SocketSettings for Settings {
+    fn socket(&self) -> SocketAddr {
+      do self.fetch_with("bind") |addr| {
+        match addr {
+          Some(socket_addr) => { socket_addr },
+          None => {
+            let port: u16 = self.port();
+            let ip: IpAddr = self.ip();
+            SocketAddr { ip: ip, port: port }
+          }
+        }
+      }
+    }
+
+    fn port(&self) -> u16 {
+      self.fetch("port").unwrap_or(8080)
+    }
+
+    fn ip(&self) -> IpAddr {
+      self.fetch("ip").unwrap_or(Ipv4Addr(127,0,0,1))
+    }
+  }
+
   #[test]
-  fn test_insert() {
+  fn test_simple_conversion() {
+    let mut settings = Settings::new();
+    settings.set("port", 12345);
+    let settings = settings.fetch("port");
+    assert_eq!(Some(12345), settings)
+  }
+
+  #[test]
+  fn test_compound_socket_settings() {
     let mut settings = Settings::new();
     settings.set("port", "12345");
     settings.set("ip", "127.0.0.1");
-    settings.socket();
+    let socket = settings.socket();
+    assert_eq!(socket.to_str(), ~"127.0.0.1:12345")
+  }
+
+  #[test]
+  fn test_socket_overrides_port() {
+    let mut settings = Settings::new();
+    settings.set("port", "12345");
+    settings.set("ip", "127.0.0.1");
+    settings.set("bind", "0.0.0.0:4567");
+    let socket = settings.socket();
+    assert_eq!(socket.to_str(), ~"0.0.0.0:4567")
+  }
+
+  #[test]
+  fn test_opt_parse_optional() {
+    let mut settings = Settings::new();
+    settings.opt(optopt("p", "port", "The port to bind to", "eg: 4000"));
+
+    let args = ~[~"myprog", ~"-p", ~"3000"];
+    let error = settings.load_args(args);
+
+    assert!(error.is_none());
+    assert_eq!(settings.fetch("port"), Some(3000))
+  }
+
+  #[test]
+  fn test_opt_parse_req_with_given() {
+    let mut settings = Settings::new();
+    settings.opt(reqopt("p", "port", "The port to bind to", "eg: 4000"));
+
+    let args = ~[~"myprog", ~"-p", ~"3000"];
+    let error = settings.load_args(args);
+
+    assert!(error.is_none());
+    assert_eq!(settings.fetch("port"), Some(3000))
+  }
+
+  #[test]
+  fn test_opt_parse_req_without_given() {
+    let mut settings = Settings::new();
+    settings.opt(reqopt("p", "port", "The port to bind to", "eg: 4000"));
+
+    let args = ~[~"myprog"];
+    let error = settings.load_args(args);
+
+    assert!(error.is_some());
+    let port: Option<int> = settings.fetch("port");
+    assert_eq!(port, None)
+  }
+
+  #[test]
+  fn test_usage() {
+    let mut settings = Settings::new();
+    settings.opt(reqopt("p", "port", "The port to bind to", "eg: 4000"));
+    let usage = settings.usage(&"this is how it works");
+
+    assert!(usage.contains("this is how it works"))
+    assert!(usage.contains("--port"))
   }
 }
