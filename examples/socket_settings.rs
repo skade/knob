efe6e90db787ea694e78afb41f8cdf0ea54511dd
moonglum <moonglum@moonbeamlabs.com>
2014-05-10 22:27:23 -0700

    Add the socket settings example from the README to examples

---
+++ examples/socket_settings.rs
@@ -0,0 +1,11 @@
+extern crate knob;
+
+use std::io::net::ip::IpAddr;
+use knob::Settings;
+
+fn main() {
+    let mut settings = Settings::new();
+    settings.set("ip", "::0.0.0.1");
+    let socket: IpAddr = settings.fetch("ip").unwrap();
+    assert_eq!(socket.to_str(), "::0.0.0.1".to_owned());
+}
